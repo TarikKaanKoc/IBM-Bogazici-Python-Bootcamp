4cd0fa1507104641107cc9bdf7e414eba4571834
Tarık Kaan Koç <83168207+TarikKaanKoc@users.noreply.github.com>
2022-11-05 22:36:40 +0300

    commit

---
+++ 1-Week/FirstProjcet/first_class.py
@@ -0,0 +1,13 @@
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+print("Hello world")
+
+i = [1,2,3,4]
+for value in i:
+    print(value)
+
